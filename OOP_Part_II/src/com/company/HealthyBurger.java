4b4bf89cb0b829441157eb38578e533d65ca0cae
Weiwei <ww.zhang@mail.utoronto.ca>
2020-12-31 17:22:37 -0500

    final OOP exercise

--- OOP_Part_II/src/com/company/HealthyBurger.java
+++ OOP_Part_II/src/com/company/HealthyBurger.java
@@ -9,4 +9,38 @@ public class HealthyBurger extends Hamburger{
     public HealthyBurger(String meat, double price) {
         super("Healthy", meat, price, "Brown");
     }
+
+    public void addHealthyAddition1(String name, double price) {
+        this.healthyExtra1Name = name;
+        this.healthyExtra1Price = price;
+    }
+
+    public void addHealthyAddition2(String name, double price) {
+        this.healthyExtra2Name = name;
+        this.healthyExtra2Price = price;
+    }
+
+    @Override
+    public double itemizeHamburger() {
+        double totalPrice = super.itemizeHamburger();
+        for(int j = 0; j < 2; j++){
+            String additionHealthyName;
+            double additionHealthyPrice;
+            switch (j) {
+                case 0:
+                    additionHealthyName = this.healthyExtra1Name;
+                    additionHealthyPrice = this.healthyExtra1Price;
+                    break;
+                default:
+                    additionHealthyName = this.healthyExtra2Name;
+                    additionHealthyPrice = this.healthyExtra2Price;
+                    break;
+            }
+            if(additionHealthyName != null){
+                System.out.println("Added " + additionHealthyName + " for an extra " + additionHealthyPrice);
+                totalPrice += additionHealthyPrice;
+            }
+        }
+        return totalPrice;
+    }
 }
