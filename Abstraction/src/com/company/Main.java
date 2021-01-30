63479bb6e413f358140fccb24bbdc461859c2423
Weiwei <ww.zhang@mail.utoronto.ca>
2021-01-30 10:48:13 -0500

    abstract class exercise

--- Abstraction/src/com/company/Main.java
+++ Abstraction/src/com/company/Main.java
@@ -3,8 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Dog Max = new Dog("Max");
-        Max.eat();
-        Max.breathe();
+        SearchTree tree = new SearchTree(null);
+        tree.traverse(tree.getRoot());
+        String stringData = "4 2 3 6 7 8 9";
+
+        String[] data = stringData.split(" ");
+        for(String s : data) {
+            tree.addItem(new Node(s));
+        }
+
+        tree.traverse(tree.getRoot());
+        tree.removeItem(new Node("3"));
+        tree.traverse(tree.getRoot());
     }
 }
