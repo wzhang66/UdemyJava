12b140a93c46ea6f98ae5972062f2248ce3866d0
Weiwei <ww.zhang@mail.utoronto.ca>
2021-02-25 22:40:05 -0500

    finish Hello world program

--- HelloWorldFX/src/sample/Main.java
+++ HelloWorldFX/src/sample/Main.java
@@ -2,8 +2,14 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,8 +17,19 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+//        The follow code will be converted to fxml automatically
+//        GridPane root = new GridPane();
+//        root.setAlignment(Pos.CENTER);
+//        root.setVgap(10);
+//        root.setHgap(10);
+//        Add hello world greeting interface
+//        Label greeting = new Label("Welcome to FX!");
+//        greeting.setTextFill(Color.GREEN);
+//        greeting.setFont(Font.font("Times New Roman", FontWeight.BOLD, 30));
+//        root.getChildren().add(greeting);
+
+        primaryStage.setTitle("Hello JavaFX!");
+        primaryStage.setScene(new Scene(root, 700, 275));
         primaryStage.show();
     }
 
