df7633c7809e33621b4e416fc4e0efe39789ec15
Weiwei <ww.zhang@mail.utoronto.ca>
2021-03-25 23:06:56 -0400

    using data binding to update the listview

--- TodoList/src/com/weiwei/todolist/Controller.java
+++ TodoList/src/com/weiwei/todolist/Controller.java
@@ -35,7 +35,7 @@ public class Controller {
             }
         });
 
-        todoListView.getItems().setAll(TodoData.getInstance().getTodoItems());
+        todoListView.setItems(TodoData.getInstance().getTodoItems());
         todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
         todoListView.getSelectionModel().selectFirst();
     }
@@ -64,12 +64,7 @@ public class Controller {
         if(result.isPresent() && result.get()==ButtonType.OK){
             DialogController controller = fxmlLoader.getController();
             TodoItem newItem = controller.processResults();
-
-            todoListView.getItems().setAll(TodoData.getInstance().getTodoItems());
             todoListView.getSelectionModel().select(newItem);
-            System.out.println("OK");
-        } else {
-            System.out.println("Cancel");
         }
     }
 
