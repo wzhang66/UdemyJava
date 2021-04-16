ee7a40d1db5fae226289ddeba6c30148fc5ca815
Weiwei <ww.zhang@mail.utoronto.ca>
2021-04-15 23:42:41 -0400

    file filters in the file and the webpage link

--- JavaFXApplication/src/module-info.java
+++ JavaFXApplication/src/module-info.java
@@ -1,6 +1,8 @@
 module JavaFXApplication {
     requires javafx.fxml;
     requires javafx.controls;
+    requires java.desktop;
+    requires javafx.web;
 
     opens sample;
 }
--- JavaFXApplication/src/sample/Controller.java
+++ JavaFXApplication/src/sample/Controller.java
@@ -5,15 +5,23 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.GridPane;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 
+import java.awt.*;
 import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
 
 public class Controller {
     @FXML private Label label;
     @FXML private Button button4;
     @FXML private GridPane gridPane;
+    @FXML private WebView webView;
 
     public void initialize(){
         button4.setEffect(new DropShadow());
@@ -33,15 +41,37 @@ public class Controller {
 
     @FXML
     public void handleClick(){
-//        FileChooser chooser = new FileChooser();
-//        chooser.showOpenDialog(gridPane.getScene().getWindow());
-        DirectoryChooser chooser = new DirectoryChooser();
-        File file = chooser.showDialog(gridPane.getScene().getWindow());
+        FileChooser chooser = new FileChooser();
+//        chooser.setTitle("Save Application File");
+        chooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Text", "*.txt"),
+                new FileChooser.ExtensionFilter("PDF", "*.pdf"),
+                new FileChooser.ExtensionFilter("All Files", "*.*")
+        );
+        List<File> file = chooser.showOpenMultipleDialog(gridPane.getScene().getWindow());
+//        DirectoryChooser chooser = new DirectoryChooser();
+//        File file = chooser.showDialog(gridPane.getScene().getWindow());
         if(file!=null){
-            System.out.println(file.getPath());
+            for(int i=0; i< file.size(); i++){
+                System.out.println(file.get(i).getPath());
+            }
+
         } else {
             System.out.println("Cancel");
         }
+    }
+
+    @FXML
+    public void handleLink(){
+//        try {
+//            Desktop.getDesktop().browse(new URI("http://www.javafx.com"));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } catch (URISyntaxException e){
+//            e.printStackTrace();
+//        }
+        WebEngine engine = webView.getEngine();
+        engine.load("http://www.javafx.com");
     }
 
 
